d65ea38023440c7a696f29a38197dd2429f89203
Devashish <official.devashishthakur@gmail.com>
2020-08-28 16:58:45 +0530

    devashish edit profile functionality

---
+++ app/src/main/java/com/pkdev/e_card/model/Social.java
@@ -0,0 +1,66 @@
+package com.pkdev.e_card.model;
+
+public class Social {
+    String facebook,instagram,linkedin,twitter,snapchat,whatsapp;
+
+    public Social(){
+
+    }
+
+    public Social(String facebook, String instagram, String linkedin, String twitter, String snapchat, String whatsapp) {
+        this.facebook = facebook;
+        this.instagram = instagram;
+        this.linkedin = linkedin;
+        this.twitter = twitter;
+        this.snapchat = snapchat;
+        this.whatsapp = whatsapp;
+    }
+
+    public String getFacebook() {
+        return facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        this.facebook = facebook;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    public String getSnapchat() {
+        return snapchat;
+    }
+
+    public void setSnapchat(String snapchat) {
+        this.snapchat = snapchat;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+}
